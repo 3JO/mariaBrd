7eeed31f726cff8eb165b07f5d307cfbc72e22d8
choljongin <jongin6095@gmail.com>
2015-12-31 17:25:38 +0900

    main 페이지 - 공지사항, qna, 자료실 합침

--- src/main/java/org/ibitu/controller/HomeController.java
+++ src/main/java/org/ibitu/controller/HomeController.java
@@ -4,10 +4,19 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
+import org.ibitu.domain.PageMaker;
+import org.ibitu.domain.SearchCriteria;
+import org.ibitu.service.BoardService;
+import org.ibitu.service.DBoardService;
+import org.ibitu.service.QBoardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,10 +28,19 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	//새로 건든거
+	@Inject
+	private BoardService service;
+	
+	@Inject
+	private QBoardService service1;
+	
+	@Autowired
+	private DBoardService service2;
+	
+	
+	  
+/*	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -34,11 +52,37 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}*/
+	
+	//새로 건든 부분
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String listPage(@ModelAttribute("cri")SearchCriteria cri,Model model)throws Exception{
+		logger.info(cri.toString());
+		
+		
+		model.addAttribute("list", service.listSearchCriteria(cri));
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		pageMaker.setTotalCnt(service.listSearchCount(cri));
+		model.addAttribute("pageMaker", pageMaker);
+		
+		
+		model.addAttribute("list1", service1.listSearchCriteria(cri));
+		PageMaker pageMaker1 = new PageMaker();
+		pageMaker1.setCri(cri);
+		pageMaker1.setTotalCnt(service1.listSearchCount(cri));
+		model.addAttribute("pageMaker1", pageMaker1);
+		
+		
+		
+		model.addAttribute("list2", service2.listSearchCriteria(cri));
+		PageMaker pageMaker2 = new PageMaker();
+		pageMaker2.setCri(cri);
+		pageMaker2.setTotalCnt(service2.listSearchCnt(cri));
+		model.addAttribute("pageMaker", pageMaker2);
+		
+		return "home";
 	}
 	
-	@RequestMapping(value="/test", method=RequestMethod.GET)
-	public void ajaxTest(){
-		
-	}
 	
 }
